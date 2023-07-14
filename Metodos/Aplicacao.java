b2e915fbd9ce6fb24187e77d970a88e76fb9fd13
narjiara <narjiarabatista447@gmail.com>
2023-07-14 16:46:42 -0300

    metodo

---
+++ Metodos/Aplicacao.java
@@ -0,0 +1,13 @@
+package Metodos;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Carro prototipoDeCarro = new Carro();
+		
+		//construtor com 2 parametros
+		Carro civicPreto = new Carro("New Civic",40000);
+		//construtor com 3 parametros
+		Carro golfAmarelo = new Carro("Preto","Golf",38000);
+	}
+
+}
---
+++ Metodos/Aplicacao2.java
@@ -0,0 +1,8 @@
+package Metodos;
+
+public class Aplicacao2 {
+	public static void main(String[] args) {
+		Honda hondaFitPreto = new Honda("2.0 flex","Honda Accord",60000);
+	}
+
+}
